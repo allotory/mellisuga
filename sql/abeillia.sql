13f46cd7af7c07ed3306216899d9a8e585d33f78
Ellery <allotory@msn.com>
2015-03-12 14:38:34 +0800

    add table question and answer

--- sql/abeillia.sql
+++ sql/abeillia.sql
@@ -19,11 +19,11 @@ CREATE TABLE member (
 	member_id int(10) unsigned NOT NULL,
 	fullname varchar(40) NOT NULL,
 	gender int(11) NOT NULL,
-	is_admin` int(2) unsigned NOT NULL DEFAULT '0',
-	avatar_path` varchar(30) DEFAULT NULL,
+	is_admin int(2) unsigned NOT NULL DEFAULT '0',
+	avatar_path varchar(30) DEFAULT NULL,
   	muted int(2) NOT NULL DEFAULT '0',
   	status int(2) NOT NULL DEFAULT '0',
-	registrationdate datetime NOT NULL,
+	registration_date datetime NOT NULL,
   	location varchar(100),
   	business varchar(50),
   	employment varchar(100),
@@ -34,7 +34,38 @@ CREATE TABLE member (
 	PRIMARY KEY (member_id)
 );
 
+/* 问题表 */
+CREATE TABLE question (
+	id int(10) NOT NULL AUTO_INCREMENT,
+	questioner_id int(10) unsigned NOT NULL,
+	question_title varchar(200) NOT NULL,
+	question_content text NOT NULL,
+	answers_num int(11) NOT NULL DEFAULT '0',
+	followers_num int(11) NOT NULL DEFAULT '0',
+	last_updated datetime NOT NULL,
+	scan_num int(11) NOT null DEFAULT '0'
+);
 
+/* 答案表 */
+CREATE TABLE answers (
+	id int(10) NOT NULL AUTO_INCREMENT,
+	question_id int(10) unsigned NOT NULL,
+	answerer_author_id int(10) unsigned NOT NULL,
+	answers text NOT NULL,
+	answer_date datetime NOT NULL
+);
+
+/* 问题评论表 */
+
+/* 问题标签表 */
+
+/* 问题日志表 */
+
+
+/* 答案赞同表 */
+/* 答案感谢表 */
+/* 答案收藏表 */
+/* 答案没有帮助表 */
 
 
 
