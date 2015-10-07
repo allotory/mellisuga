256fb54b3211c6bce20d892528eb77ec427cd03e
Ellery <allotory@msn.com>
2015-10-07 16:00:06 +0800

    Mod: change comment question from out html to out json

--- src/com/mellisuga/servlet/CommentQuestionServlet.java
+++ src/com/mellisuga/servlet/CommentQuestionServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
 
+import com.mellisuga.bean.CommentBean;
 import com.mellisuga.dao.CommentDAO;
 import com.mellisuga.dao.QuestionDAO;
 import com.mellisuga.db.DBConnection;
@@ -46,6 +50,8 @@ public class CommentQuestionServlet extends HttpServlet {
 				.format(date);
 		Timestamp now = Timestamp.valueOf(dateFormate);
 		
+		CommentBean commentBean = new CommentBean();
+		
 		SqlSession session = null;
 		try {
 			session = DBConnection.openDefaultSession();
@@ -75,43 +81,24 @@ public class CommentQuestionServlet extends HttpServlet {
 			// 提交
 			session.commit();
 			
+			commentBean.setComment(comment);
+			commentBean.setMember(m);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
 			DBConnection.closeSession(session);
 		}
-
+		
+		List<CommentBean> commentBeanList = new ArrayList<CommentBean>();
+		commentBeanList.add(commentBean);
+		
 		// 返回评论
-		out.print("<div class='panel-body q-comment'>");
-		out.print("<div class='avatar-vote col-lg-1 col-md-1 col-sm-1 col-xs-1'>");
-		out.print("<div class='row'>");
-		out.print("<a href='#'>");
-		out.print("<img src='" + m.getAvatar_path() + "' class='img-responsive img-rounded' alt='Responsive image'>");
-		out.print("</a>");
-		out.print("</div>");
-		out.print("</div><!-- end avatar and upvote col -->");
-		out.print("<div class='content-details col-lg-11 col-md-11 col-sm-10 col-xs-10'>");
-		out.print("<div class='row'>");
-		out.print("<div class='author-info'><a href='#'><strong>" + m.getFullname() + "</strong></a></div>");
-		out.print("</div>");
-		out.print("<div class='row'>");
-		out.print("<div class='question-content'>");
-		out.print("<div class='editable-content' style='display: block;'>");
-		out.print(content);
-		out.print("<span class='answer-date' style='display: block;'>");
-		out.print("<a target='_blank' href='#'>发布于 " + now + "</a>");
-		out.print("<span style='margin-left: 5px;'>");
-		out.print("<a href='#' class='split'><i class='fa fa-reply'></i> 回复</a>");
-		out.print("<a href='#' class='split'><i class='fa fa-thumbs-o-up'></i> 赞</a>");
-		out.print("<a href='#' class='split'><i class='fa fa-flag-o'></i> 举报</a>");
-		out.print("</span>");
-		out.print("<a href='#' class='split module-right'>0赞</a>");
-		out.print("</span>");
-		out.print("</div>");
-		out.print("</div>");
-		out.print("</div>");
-		out.print("</div><!-- end comment-details -->");
-		out.print("</div>");
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("commentBeanList", commentBeanList);
+		
+		out.print(jsonObject);
+
 	}
 
 	protected void doPost(HttpServletRequest request,
