bfa2426eca2ef4c32779d4058da42995d526ccc7
Ellery <allotory@msn.com>
2015-12-08 21:53:43 +0800

    Fixbug: fix NaN bug when up=down=0

--- src/com/mellisuga/servlet/QuestionDetailsServlet.java
+++ src/com/mellisuga/servlet/QuestionDetailsServlet.java
@@ -114,26 +114,6 @@ public class QuestionDetailsServlet extends HttpServlet {
 					answerBean.setAnswer(a);
 					
 					// 计算并设置答案权重
-					/**
-					 *  威尔逊区间算法
-					 *  	u 为加权赞同票数，
-					 *  	v 为加权反对票数，
-					 *  	z_alpha 为参数， 暂定2.2，(5~10好一点，吗)
-					 * 
-					 *  n = u + v;
-					 *  p = u / n;
-					 *  
-					 *  score = (p + z*z/2n - (z/2n)*Math.sqrt(4n*(1 - p)*p + z*z)) 
-					 *  			/ (1 + z*z/n)
-					 *  
-					 *  z设置得越高，绝对赞同数就越重要，反之设置较低时，赞同率就越重要。
-					 *  当z等于0时，赞同率将起到全部作用。
-					 *  比如一个1000赞，100反对的回答，当z等于0.73时，其分数将完全等于一个1赞0反对的回答。
-					 *  
-					 *  当z增大时，绝对赞同数起的作用就开始增大。
-					 *  两个回答，前者100赞30反对，后者70赞0反对，那么当z小于21时，后者排名靠前，当大于21时，前者排名靠前。
-					 */
-					
 					// 赞同数
 					HashMap<String, Object> voteUpCountMap = new HashMap<String, Object>();
 					voteUpCountMap.put("answer_id", a.getId());
@@ -147,7 +127,12 @@ public class QuestionDetailsServlet extends HttpServlet {
 					int voteDownCount = voteDAO.queryCountByAidVoteType(voteDownCountMap);
 					
 					// 计算权重
-					double answerWeight = WilsonScoreInterval.getWSI(voteUpCount, voteDownCount);
+					double answerWeight = 0.0d;
+					if(voteUpCount == 0 && voteDownCount == 0) {		
+						answerWeight = 0.0d;
+					} else {
+						answerWeight = WilsonScoreInterval.getWSI(voteUpCount, voteDownCount);
+					}
 					answerBean.setAnswerWeight(answerWeight);
 					
 					
